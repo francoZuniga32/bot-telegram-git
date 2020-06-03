6a8976766399fde5ac26a60395b86c4e48138473
franco <franco.ojeda@est.fi.uncoma.edu.ar>
2020-06-02 23:58:56 -0300

    d0206h2359

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const Telegraf = require('telegraf');
+var os = require("os");
+const drivelist = require('drivelist');
+
 let TOKEN = require('./configuration');
 
 
 const bot = new Telegraf(TOKEN.token);
 
+//comandos comunes
 bot.start((contexto) => {
     contexto.reply("hola wacho!!");
 });
@@ -16,10 +20,38 @@ bot.settings((contexto) => {
     contexto.reply("a configurar el bot!!");
 });
 
+//comandos pesonalizados
 bot.command('git', (contexto)=>{
     contexto.reply("a trabajar");
 });
 
+bot.command('os', (contexto)=>{
+    console.log(os.cpus());
+    contexto.reply(`Cpus: ${os.cpus().model} , Memoria (Kb): ${os.totalmem()}, Memoria libre (Kb) ${os.freemem()}`);
+});
 
+
+//eventos de escucha y demas
+bot.hears('computer', (contexto) => {
+    contexto.reply('ha escrito coputer');
+});
+
+//cosas especiales de contexto
+
+bot.command('nickuser', (contexto) => {
+    console.log(contexto.from);
+    console.log(contexto.chat);
+    console.log(contexto.message);
+    contexto.reply('buenas '+contexto.from.first_name);
+});
+
+/*bot.context.db = {
+  getScores: () => { return 42 }
+}
+
+bot.on('text', (ctx) => {
+  const scores = ctx.db.getScores(ctx.message.from.username)
+  return ctx.reply(`${ctx.message.from.username}: ${scores}`)
+})*/
 
 bot.launch();
